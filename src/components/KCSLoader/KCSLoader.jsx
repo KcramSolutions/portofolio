b5583e8e454c9cc1bfc48f995710a665cbb497ea
Marck Carrión Guzmán <mkcarrrion@kcramsolutions.com>
2023-08-21 10:22:23 +0200

    feat: creado loader

---
+++ src/components/KCSLoader/KCSLoader.jsx
@@ -0,0 +1,6 @@
+import react from 'react';
+import './KCSLoader.css';
+ export function KCSLoader({size}){
+	let dimension = size?? 3;
+	 return (<div style={{width: dimension + "rem", height: dimension + "rem"}} className='kcs-loader'></div>)
+}
